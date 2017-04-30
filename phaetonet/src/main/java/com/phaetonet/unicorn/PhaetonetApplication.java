54d4ffba988e87d56dbcf804659bb0a7dd13b371
baozhuohui <baozhuohui@126.com>
2017-05-01 01:08:41 +0800

    init

---
+++ phaetonet/src/main/java/com/phaetonet/unicorn/PhaetonetApplication.java
@@ -0,0 +1,12 @@
+package com.phaetonet.unicorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaetonetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhaetonetApplication.class, args);
+	}
+}
