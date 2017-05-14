792664ccec4e717bcb1f377f4fb73384cd480517
baozhuohui <baozhuohui@126.com>
2017-05-15 00:56:46 +0800

    add login

---
+++ phaetonet/phaetonet-login/src/main/java/com/phaetonet/unicorn/login/ServletInitializer.java
@@ -0,0 +1,26 @@
+package com.phaetonet.unicorn.login;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(APP.class);
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext)
+            throws ServletException {
+        FilterRegistration.Dynamic openEntityManagerInViewFilter = servletContext.addFilter("openEntityManagerInViewFilter", OpenEntityManagerInViewFilter.class);
+        openEntityManagerInViewFilter.setInitParameter("entityManagerFactoryBeanName","entityManagerFactory");
+        openEntityManagerInViewFilter.addMappingForUrlPatterns(null, false, "/*");
+        super.onStartup(servletContext);
+    }
+}
---
+++ phaetonet/phaetonet-login/src/main/java/com/phaetonet/unicorn/login/support/Appctx.java
@@ -0,0 +1,10 @@
+package com.phaetonet.unicorn.login.support;
+
+import org.springframework.context.ApplicationContext;
+
+public class Appctx {
+    public static ApplicationContext ctx=null;
+    public static Object getObject(String string){
+        return ctx.getBean(string);
+    }
+}
